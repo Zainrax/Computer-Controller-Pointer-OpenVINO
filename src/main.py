0c8a2e98dc568d2c3a770832b1be8efc9c46e91c
zainrax <patrickb321@gmail.com>
2020-06-30 16:52:03 +1200

    feat(main): initial file

---
+++ src/main.py
@@ -0,0 +1,19 @@
+"""
+Tasked with handling CLI interaction & performing the model
+parsing to provide the proper output. Takes in OpenVINO models
+for face detection to determine boundry, from which the cropped
+head is infered to find the head pose & landmarks, fed into
+the gaze model to find where a person is looking, controlling
+where the point will be placed.
+
+Author: Patrick Baxter
+Date: 30/06/2020
+License: MIT
+"""
+
+
+def main():
+    print("hello")
+
+if __name__ == '__main__':
+    main()
