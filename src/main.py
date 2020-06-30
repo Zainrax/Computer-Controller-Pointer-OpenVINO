57ae6b54df520ccf3e2bde9f02b8b8ef46097bf8
zainrax <patrickb321@gmail.com>
2020-06-30 22:04:36 +1200

    feat(main): add argparser

--- src/main.py
+++ src/main.py
@@ -10,10 +10,66 @@ Author: Patrick Baxter
 Date: 30/06/2020
 License: MIT
 """
+from argparse import ArgumentParser
 
+def build_argparser():
+    """
+    Parse command line arguments.
+    :return: command line arguments
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-fd",
+                        "--face_detection",
+                        required=True,
+                        type=str,
+                        help="Path to an xml file for the face detection model.")
+    parser.add_argument("-fl",
+                        "--face_landmark",
+                        required=True,
+                        type=str,
+                        help="Path to an xml file for the face landmark model.")
+    parser.add_argument("-g",
+                        "--gaze_detection",
+                        required=True,
+                        type=str,
+                        help="Path to an xml file for the gaze estimation model.")
+    parser.add_argument("-p",
+                        "--pose_detection",
+                        required=True,
+                        type=str,
+                        help="Path to an xml file face pose detection model.")
+    parser.add_argument("-i",
+                        "--input",
+                        required=True,
+                        type=str,
+                        help="Path to video file, or cam for Web Cam Usage.")
+    parser.add_argument("-pt",
+                        "--prob_threshold",
+                        type=float,
+                        default=0.5,
+                        help="Probability threshold for detections filtering"
+                        "(0.5 by default)")
+    parser.add_argument("-l",
+                        "--cpu_extension",
+                        required=False,
+                        type=str,
+                        default=None,
+                        help="MKLDNN (CPU)-targeted custom layers."
+                        "Absolute path to a shared library with the"
+                        "kernels impl.")
+    parser.add_argument("-d",
+                        "--device",
+                        type=str,
+                        default="CPU",
+                        help="Specify the target device to infer on: "
+                        "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
+                        "will look for a suitable plugin for device "
+                        "specified (CPU by default)")
+    return parser
 
-def main():
-    print("hello")
+def main(args):
+    print(args.device)
 
 if __name__ == '__main__':
+    args = build_argparser().parse_args()
     main()
